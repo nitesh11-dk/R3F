0752fdffe9538b8147751264057c1d0c56cf818f
Nitesh-DK <nittesh.dk@gmail.com>
2024-12-20 11:43:07 +0530

    Introduce ReactHooks component and update App component structure
    - Added a new ReactHooks component for interactive 3D cube manipulation using React Three Fiber and Leva.
    - Updated App component to include ReactHooks and commented out the Shaders component for future use.
    - Enhanced the user experience by allowing color changes to the cube through GUI controls.

--- WS/src/App.jsx
+++ WS/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Intro from './Components/Intro'
 import Lights from './Components/Lights'
 import Shaders from './Components/Shaders'
+import ReactHooks from './Components/ReactHooks'
 const App = () => {
   return (
     <div className='h-screen w-screen bg-zinc-900'>
       {/* <Intro /> */}
       {/* <Lights /> */}
-      <Shaders/>
+      {/* <Shaders/> */}
+      <ReactHooks />
     </div>
   )
 }
---
+++ WS/src/Components/ReactHooks.jsx
@@ -0,0 +1,49 @@
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useState ,useMemo } from "react";
+import { useControls ,button} from "leva";
+const Cube = (props) => {
+    const [color, setColor] = useState("white");
+
+    useControls({
+      changeColorToRed: button(() => setColor("red")),
+      changeColorToBlue: button(() => setColor("blue")),
+      changeColorToGreen: button(() => setColor("green")),
+    });
+    
+    const material = useMemo(() => <meshStandardMaterial color={color} />, [color]);
+    
+                
+  return (
+    <mesh {...props}>
+      <boxGeometry />
+      {material}
+    </mesh>
+  );
+};
+
+function App() {
+
+
+
+    
+
+  return (
+    <>
+      <Canvas camera={{ position: [0, 2, 6], fov: 42 }}>
+        <OrbitControls />
+        <Cube rotation-y={Math.PI / 4} />
+        <ContactShadows
+          position-y={-2}
+          opacity={0.5}
+          blur={2}
+          color={"pink"}
+          scale={10}
+        />
+        <Environment preset="city" />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
