b56d1bbdc740eb903b9708d267916fd07f7e3e24
Nitesh-DK <nittesh.dk@gmail.com>
2024-12-25 21:20:40 +0530

    Enhance Controls component with new 3D interaction features
    - Added imports for PivotControls and PresentationControls from @react-three/drei to improve user interaction within the 3D scene.
    - Commented out various configurations for OrbitControls and PresentationControls for future customization.
    - Wrapped the Lighthouse model with PivotControls to enable better manipulation in the 3D environment.
    - Updated the structure of the Controls component to facilitate enhanced scene interaction.

--- WS/2.Core/src/Components/Controls.jsx
+++ WS/2.Core/src/Components/Controls.jsx
@@ -1,4 +1,4 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls , PivotControls, PresentationControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Lighthouse } from "./Lighthouse";
 
@@ -8,8 +8,36 @@ function Controls() {
   return (
     <>
       <Canvas camera={{ position: [-1.5, 3, 10], fov: 42 }}>
-        <OrbitControls />
-        <Lighthouse position-y={-1} scale={[0.2, 0.2, 0.2]} />
+        {/* <OrbitControls 
+        enableZoom={false}
+        rotateSpeed={0.5}
+        /> */}
+
+        {/* <OrbitControls
+        enablePan={false} minPolarAngle={Math.PI/2} maxPolarAngle={Math.PI/2} minAzimuthAngle={-Math.PI/2} maxAzimuthAngle={Math.PI/2} minDistance={3} maxDistance={10}
+        /> */}
+
+
+       {/* <PresentationControls
+  enabled={true} // the controls can be disabled by setting this to false
+  global={false} // Spin globally or by dragging the model
+  cursor={true} // Whether to toggle cursor style on drag
+  snap={0.7} // Snap-back to center (can also be a spring config)
+  speed={1} // Speed factor
+  zoom={1} // Zoom factor when half the polar-max is reached
+  rotation={[0, 0, 0]} // Default rotation
+  polar={[0, Math.PI / 2]} // Vertical limits
+  azimuth={[-Infinity, Infinity]} // Horizontal limits
+  config={{ mass: 1, tension: 170, friction: 26 }} // Spring config
+>        
+</PresentationControls> */}
+
+<PivotControls>
+
+<Lighthouse position-y={-1} scale={[0.2, 0.2, 0.2]} />
+
+</PivotControls>
+
         <Environment preset="sunset" />
       </Canvas>
     </>
