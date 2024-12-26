aa9ee8d7909bc00b50c9fca8b134fc7460e93352
Nitesh-DK <nittesh.dk@gmail.com>
2024-12-26 10:27:18 +0530

    Add foundational files and components for React Three Fiber project
    - Introduced ESLint configuration in .eslintrc.cjs for linting React code.
    - Created .gitignore to exclude unnecessary files and directories.
    - Established initial HTML structure in index.html for the application.
    - Developed package.json with essential dependencies for React, Vite, and ESLint.
    - Implemented main entry point in main.jsx and styling in index.css.
    - Added core components: App, Experience, AnimatedBox, AnimatedDodecahedron, Background, Duck, and Teeth for 3D rendering using React Three Fiber.
    - Configured Vite in vite.config.js for development and build processes.
    - Included public assets such as vite.svg and 3D models for rendering.

---
+++ WS/2.Core/20-animations-starter/src/components/Background.jsx
@@ -0,0 +1,46 @@
+import { useRef , useEffect } from "react";
+import * as THREE from "three";
+import gsap from "gsap";
+import { useFrame } from "@react-three/fiber";
+import { useScroll } from "@react-three/drei";
+export const Background = () => {
+  const skyMaterial = useRef();
+  const tl = useRef();
+  const skyData = useRef({
+    color: "#313131",
+  });
+
+  useEffect(() => {
+    tl.current = gsap.timeline();
+    tl.current
+      .to(skyData.current, {
+        duration: 1,
+        color: "#ffc544",
+      })
+      .to(skyData.current, {
+        duration: 1,
+        color: "#7c4e9f",
+      });
+  }, []);
+
+  const scrollData = useScroll();
+  useFrame(() => {
+    if (!tl.current) {
+      return;
+    }
+    // skyMaterial.current.color.set(skyData.current.color);
+
+    tl.current.progress(scrollData.offset);
+    skyMaterial.current.color.set(skyData.current.color);
+  });
+  return (
+    <mesh rotation-x={Math.PI / 4}>
+      <sphereGeometry args={[16, 32, 32]} />
+      <meshBasicMaterial
+        side={THREE.BackSide}
+        color={"#313131"}
+        ref={skyMaterial}
+      />
+    </mesh>
+  );
+};
---
+++ WS/2.Core/20-animations-starter/src/components/Experience.jsx
@@ -0,0 +1,54 @@
+import { Duck } from "./Duck";
+
+import { Scroll } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { AnimatedBox } from "./AnimatedBox";
+import { AnimatedDodecahedron } from "./AnimatedDodecahedron";
+import { Background } from "./Background";
+import { Teeth } from "./Teeth";
+import { Float } from "@react-three/drei";
+export const Experience = () => {
+  const viewport = useThree((state) => state.viewport);
+
+  return (
+    <>
+      <group position-y={-0.75}>
+      <Float floatIntensity={2} speed={4}>
+          <Duck />
+        </Float>
+      </group>
+      <Background />
+      <Scroll>
+        {/* FIRST SECTION */}
+        <group position-z={-2}>
+          <AnimatedBox
+            boxPositions={[
+              { x: -2, y: 0, z: 0 },
+              { x: 0, y: 2, z: 0 },
+              { x: 2, y: 0, z: 0 },
+              { x: 0, y: -2, z: 0 },
+            ]}
+          />
+          <AnimatedBox
+            boxPositions={[
+              { x: 2, y: 0, z: 0 },
+              { x: 0, y: -2, z: 0 },
+              { x: -2, y: 0, z: 0 },
+              { x: 0, y: 2, z: 0 },
+            ]}
+          />
+        </group>
+
+        {/* SECOND SECTION */}
+        <group position-y={-viewport.height} position-z={1}>
+          <Teeth />
+        </group>
+
+        {/* THIRD SECTION */}
+        <group position-y={-viewport.height * 2} position-z={-1}>
+          <AnimatedDodecahedron />
+        </group>
+      </Scroll>
+    </>
+  );
+};
