e977f320cea0650bd9c1f6e74db5f673d2f6ca89
Nitesh-DK <nittesh.dk@gmail.com>
2024-12-26 09:38:55 +0530

    Add foundational files and components for loading screen starter project
    - Introduced ESLint configuration in .eslintrc.cjs for linting React code.
    - Created .gitignore to exclude unnecessary files and directories.
    - Established initial HTML structure in index.html for the application.
    - Developed package.json with essential dependencies for React, Vite, and ESLint.
    - Implemented main entry point in main.jsx and styling in index.css.
    - Added core components: App, Experience, and character models for 3D rendering using React Three Fiber.
    - Configured Vite in vite.config.js for development and build processes.
    - Included public assets such as vite.svg, 3D models, and font files for rendering.

---
+++ WS/2.Core/18-loading-screen-starter/src/App.jsx
@@ -0,0 +1,47 @@
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+import { Suspense } from "react";
+import { useProgress } from "@react-three/drei";
+const CubeLoader = () => {
+  return (
+    <mesh>
+      <boxGeometry />
+      <meshNormalMaterial />
+    </mesh>
+  );
+};
+
+const LoadingScreen = () => {
+  const { progress, active } = useProgress();
+
+  return (
+    <div className={`loading-screen ${active ? "" : "loading-screen--hidden"}`}>
+      <div className="loading-screen__container">
+        <h1 className="loading-screen__title">3D Web Agency</h1>
+        <div className="progress__container">
+          <div
+            className="progress__bar"
+            style={{ width: `${progress}%` }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <>
+    <LoadingScreen />
+      <Canvas camera={{ position: [-4, 4, 12], fov: 30 }}>
+        <Suspense fallback={<CubeLoader />}>
+          <group position-y={-1}>
+            <Experience />
+          </group>
+        </Suspense>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
