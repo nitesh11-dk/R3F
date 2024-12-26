a42b0758f4dbb9c1718d93f387bf278f7275721e
Nitesh-DK <nittesh.dk@gmail.com>
2024-12-26 09:39:07 +0530

    Refactor App component to streamline loading screen functionality
    - Removed fallback loader from Suspense in App component for improved loading behavior.
    - Updated structure to enhance readability and maintainability of the component.

--- WS/2.Core/18-loading-screen-starter/src/App.jsx
+++ WS/2.Core/18-loading-screen-starter/src/App.jsx
@@ -34,7 +34,8 @@ function App() {
     <>
     <LoadingScreen />
       <Canvas camera={{ position: [-4, 4, 12], fov: 30 }}>
-        <Suspense fallback={<CubeLoader />}>
+        <Suspense >
+        {/* <Suspense fallback={<CubeLoader />}> */}
           <group position-y={-1}>
             <Experience />
           </group>
